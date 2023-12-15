e42ed2b1f3e631bbb302f9488e1e2277d69f8191
Nitheesh26 <nitheeshprabhu2001@gmail.com>
2023-12-15 15:01:34 +0530

    learning with express

--- server.js
+++ server.js
@@ -1,72 +1,18 @@
-const http = require("http");
-const port = 8081;// port number
+const express = require("express");
 
-const toDoList =["hey everyone", "hope u all", "are doing" , "good"]; // array
-// i'm creating the server...
-http.createServer((req,res)=>{
+//initialization application
+const app = express();
+//application will now use only json data format.
+app.use(express.json());
 
-   // it is nothing but a cll back function.
-    const{method,url}=req;  // this line is nothing but what is the mthod of the particular request and what is the url of the particular req.
-    // console.log(method,url);
+const port =8081;
+toDoList = ["hey everyone","hope you"," are doing", "good"];
 
-    //url(validation)
-   if(url ==="/todos"){
-      //method (validation)
-      if(method ==="GET"){
-      console.log("todos route, and it is a get method")
-      res.writeHead(200);
-      res.write(toDoList.toString())  // here we should convert array into string other wise server will not work and nodemon will crash
-      }else if(method==="POST"){
-         let body="";
-         req.on('error',(err)=>{
-            console.log(err)
-         }).on('data',(chunk)=>{
-            body+=chunk;
-            console.log("chunk", chunk);
+app.get("/todos",(req,res)=>{
+  //callback
+  res.status(200).send(toDoList)
+})
 
-         }).on('end',()=>{
-            body=JSON.parse(body);
-            console.log("body", body);
-            let newToDO = toDoList;
-            newToDO.push(body.item);
-         })
-      }else if(method==="PUT"){
-
-   }else if(method ==="DELETE"){
-      let body ="";
-      req.on('error',(err)=>{
-         console.log(err)
-      }).on('data',(chunk)=>{
-         body+=chunk;
-      }).on('end',()=>{
-         body= JSON.parse(body);
-        let deleteThis = body.item;
-
-      //   for(let i=0; i<toDoList.length; i++){
-      //    if(toDoList[i]===deleteThis){
-      //       toDoList.splice(i,1);
-      //       break;
-      //    }
-      //   }
-      toDoList.find((elem,index)=>{
-         if(elem===deleteThis){
-            toDoList.splice(index,1)
-         }
-      })
-    })
-   }
- }
-   
-   
-   
-   else if(url ==="/"){
-   console.log("/ home default route")
-   }
-   res.end();
-}) 
-
-//server will listen the port number.
 .listen(port,()=>{
-    console.log(`Nodejs Server started Running on Port ${port}`)
-});
-//http://localhost:8081/
+   console.log(`Node javascript express started running successfully on port ${port}`)
+})
